69f66d68a08ac8a86c94876746bde580ba196c6a
theploddder <126307116+theploddder@users.noreply.github.com>
2023-12-23 08:39:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-// nodemailer.js
-
+const express = require('express');
 const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+const app = express();
+const port = 3000;
+
+// Create a transporter
 const transporter = nodemailer.createTransport({
   host: 'smtp.titan.email',
   port: 587,
@@ -12,7 +17,16 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-const sendEmail = async (recipient, subject, message) => {
+// Use the body-parser middleware
+app.use(bodyParser.json());
+
+// Enable CORS for all routes
+app.use(cors());
+
+app.post('/send-email', (req, res) => {
+  const { recipient, subject, message } = req.body;
+
+  // Configure the email details
   const mailOptions = {
     from: 'QDATA <support@qdata.com.ng>',
     to: recipient,
@@ -20,14 +34,21 @@ const sendEmail = async (recipient, subject, message) => {
     html: message
   };
 
-  try {
-    const info = await transporter.sendMail(mailOptions);
-    console.log('Email sent: ' + info.response);
-    return 'Email sent successfully';
-  } catch (error) {
-    console.error('Error sending email:', error);
-    throw new Error('Error sending email');
-  }
-};
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("error: " + error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('Email sent successfully');
+    }
+  });
+});
 
-module.exports = { sendEmail };
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+i don't want to strart any server i just want to send the email
