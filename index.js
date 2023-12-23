97ab247bfcd700f1ac6a021f6bf5caaa8b1e0ced
theploddder <126307116+theploddder@users.noreply.github.com>
2023-12-23 23:29:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,29 +2,20 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const Imap = require('imap');
 
 const app = express();
 const port = 3000;
 
-// Create a transporter for Titan Email
+// Create a nodemailer transporter for Titan Email
 const transporter = nodemailer.createTransport({
   host: 'smtp.titan.email',
-  port: 587, // Use 587 for STARTTLS
-  secure: false, // Use false for STARTTLS
+  port: 465,
+  secure: true,
   auth: {
     user: 'support@qdata.com.ng', // Replace with your email address
-    pass: 'fs?jN8YJ9URvW_*' // Replace with your email password
-  }
-});
-
-// Place the transporter.verify() call here, right after creating the transporter
-transporter.verify((error, success) => {
-    if (error) {
-        console.log(error);
-        // Handle errors as needed, e.g., display an error message to the user
-    } else {
-        console.log('Server is ready to send emails');
-    }
+    pass: 'fs?jN8YJ9URvW_*', // Replace with your email password
+  },
 });
 
 
@@ -34,7 +25,7 @@ app.use(bodyParser.json());
 // Enable CORS for all routes
 app.use(cors());
 
-app.post('/send-email', (req, res) => {
+app.post('/send-email', async (req, res) => {
   const { recipient, subject, message } = req.body;
 
   // Configure the email details
@@ -42,19 +33,58 @@ app.post('/send-email', (req, res) => {
     from: 'QDATA <support@qdata.com.ng>',
     to: recipient,
     subject: subject,
-    html: message
+    html: message,
   };
 
-  // Send the email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log("error: " + error);
-      res.status(500).send('Error sending email');
-    } else {
-      console.log('Email sent: ' + info.response);
-      res.send('Email sent successfully');
-    }
-  });
+  try {
+    // Send the email
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully.');
+    console.log('Info object:', info);
+
+    // Append the sent email to the "Sent" folder using IMAP
+    const imap = new Imap({
+      user: 'support@qdata.com.ng', // Replace with your email address
+      password: 'fs?jN8YJ9URvW_*', // Replace with your email password
+      host: 'imap.titan.email',
+      port: 993,
+      tls: true,
+    });
+
+    imap.once('ready', () => {
+      imap.openBox('Sent', true, (err) => {
+        if (err) {
+          console.error('Error opening "Sent" folder:', err);
+          imap.end();
+          return res.status(500).send('Error sending email');
+        }
+
+        // Create the email message as MIMEText
+        const emailMessage = `From: ${mailOptions.from}\r\nTo: ${mailOptions.to}\r\nSubject: ${mailOptions.subject}\r\n\r\n${mailOptions.html}`;
+
+        // Append the sent email to the "Sent" folder
+        imap.append(emailMessage, { mailbox: 'Sent' }, (appendErr) => {
+          if (appendErr) {
+            console.error('Error appending email to "Sent" folder:', appendErr);
+            res.status(500).send('Error sending email');
+          } else {
+            console.log('Email appended to "Sent" folder.');
+            res.send('Email sent successfully');
+          }
+          imap.end();
+        });
+      });
+    });
+
+    imap.once('error', (imapErr) => {
+      console.error('IMAP Error:', imapErr);
+    });
+
+    imap.connect();
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).send('Error sending email');
+  }
 });
 
 // Start the server
